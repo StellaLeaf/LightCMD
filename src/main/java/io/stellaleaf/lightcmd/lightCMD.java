6385c1409d809573537be256e16abb967efe8ca7
StellaLeaf <122457097+StellaLeaf@users.noreply.github.com>
2023-09-21 01:02:38 +0900

    Add LightCMD v1.0.1
    setting about permissions

---
+++ src/main/java/io/stellaleaf/lightcmd/lightCMD.java
@@ -0,0 +1,68 @@
+package io.stellaleaf.lightcmd;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import ru.beykerykt.lightapi.LightAPI;
+import ru.beykerykt.lightapi.LightType;
+import ru.beykerykt.lightapi.chunks.ChunkInfo;
+
+public class lightCMD extends JavaPlugin {
+	public void setLight(final Location location, int lightLevel, final LightType lightType) {
+        if (!LightAPI.isSupported(location.getWorld(), lightType)) return;
+
+        Block block = location.getBlock();
+        
+        int oldLightLevel = lightType == LightType.BLOCK ? block.getLightFromBlocks() : block.getLightFromSky();
+
+        if (oldLightLevel == lightLevel) return;
+
+        if (lightLevel > 0) {
+            LightAPI.createLight(location, lightType, lightLevel, false);
+//            if(lightTime != 0 ) {
+//                getServer().getScheduler().runTaskLater(this, new Runnable() {
+//                @Override
+//                    public void run() {
+//                    	LightAPI.deleteLight(location, lightType, false);
+//                    }
+//                }, lightTime * 20);
+//            }
+        } else {
+            LightAPI.deleteLight(location, lightType, false);
+        }
+        for (ChunkInfo chunkInfo : LightAPI.collectChunks(location, lightType, Math.max(lightLevel, oldLightLevel))) {
+            LightAPI.updateChunk(chunkInfo, lightType);
+        }
+    }
+	
+	public void onEnable() {
+		getLogger().info("LightCMD is Loaded");
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (args.length != 0) {
+			try {
+				double locX = Double.parseDouble(args[0]);
+				double locY = Double.parseDouble(args[1]);
+				double locZ = Double.parseDouble(args[2]);
+				String worldName = args[3];
+				int lightLevel = Integer.parseInt(args[4]);
+				LightType lightType = LightType.BLOCK;
+				if (args[5] == "SKY") {
+					lightType = LightType.SKY;
+				}
+//				int lightTime = Integer.parseInt(args[6]);
+				Location setLocation = new Location(Bukkit.getWorld(worldName),locX,locY,locZ);
+				setLight(setLocation, lightLevel, lightType);
+			} catch (NumberFormatException e) {
+				sender.sendMessage("[LightCMD] /light x y z world lightLevel BLOCK/SKY");
+			}
+				return true;
+		}
+		sender.sendMessage("[LightCMD] /light x y z world lightLevel BLOCK/SKY");
+		return false;
+	}
+}
